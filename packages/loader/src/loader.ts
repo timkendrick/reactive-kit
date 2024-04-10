200ea8eaba13c248c81825334203fdf93f25f1d7
Tim Kendrick <timkendrick@gmail.com>
2024-04-10 16:08:12 +0100

    Transpile reactive JSX components in custom ESM loader

--- packages/loader/src/loader.ts
+++ packages/loader/src/loader.ts
@@ -13,7 +13,15 @@ export function resolve(
   context: ResolverContext,
   next: ResolverNext,
 ): Promise<ResolverResult> {
-  return Promise.resolve(next(specifier, context));
+  return Promise.resolve(next(specifier, context)).then((result) => {
+    if (!result.format && isJsxImportSpecifier(specifier)) {
+      return {
+        ...result,
+        format: 'module',
+      };
+    }
+    return result;
+  });
 }
 
 export function load(url: string, context: LoaderContext, next: LoaderNext): Promise<LoaderResult> {
@@ -21,9 +29,17 @@ export function load(url: string, context: LoaderContext, next: LoaderNext): Pro
     if (result.source == null) return result;
     switch (result.format) {
       case 'commonjs':
-        return transpileLoaderResult(result, { url, sourceType: 'script' });
+        return transpileLoaderResult(result, {
+          url,
+          sourceType: 'script',
+          jsx: isJsxImportSpecifier(url),
+        });
       case 'module':
-        return transpileLoaderResult(result, { url, sourceType: 'module' });
+        return transpileLoaderResult(result, {
+          url,
+          sourceType: 'module',
+          jsx: isJsxImportSpecifier(url),
+        });
       case 'builtin':
       case 'json':
       case 'wasm':
@@ -33,11 +49,17 @@ export function load(url: string, context: LoaderContext, next: LoaderNext): Pro
   });
 }
 
+function isJsxImportSpecifier(specifier: string): boolean {
+  if (specifier.endsWith('.jsx')) return true;
+  if (specifier.endsWith('.tsx')) return true;
+  return false;
+}
+
 function transpileLoaderResult(
   result: LoaderResult,
-  options: { url: string; sourceType: 'script' | 'module' },
+  options: { url: string; sourceType: 'script' | 'module'; jsx: boolean },
 ) {
-  const { url, sourceType } = options;
+  const { url, sourceType, jsx } = options;
   return {
     ...result,
     source:
@@ -45,6 +67,7 @@ function transpileLoaderResult(
         filename: url,
         parser: {
           sourceType,
+          jsx,
         },
         sourcemap: true,
       }) ?? result.source,
