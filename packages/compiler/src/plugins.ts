d89d0db49220e9f44a37dfdfa38cdb3963096cf4
Tim Kendrick <timkendrick@gmail.com>
2024-04-10 16:08:03 +0100

    Transpile reactive JSX components in compiler

--- packages/compiler/src/plugins.ts
+++ packages/compiler/src/plugins.ts
@@ -1,6 +1,7 @@
 import { type PluginItem } from '@babel/core';
+import reactiveJsx from '@reactive-kit/babel-plugin-reactive-jsx';
 import reactiveFunctions from '@reactive-kit/babel-plugin-reactive-functions';
 
-const plugins: Array<PluginItem> = [reactiveFunctions];
+const plugins: Array<PluginItem> = [reactiveJsx, reactiveFunctions];
 
 export default plugins;
