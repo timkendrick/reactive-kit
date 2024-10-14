3ce9152bd9a9d8374de2872a1f880be07b7ca882
Tim Kendrick <timkendrick@gmail.com>
2024-10-14 21:10:38 +0100

    Update JSX example template

--- packages/examples/src/example.vite.config.ts
+++ packages/examples/src/example.vite.config.ts
@@ -1,7 +1,17 @@
 import { babel } from '@rollup/plugin-babel';
-import { defineConfig, mergeConfig } from 'vite';
+import { readFileSync } from 'node:fs';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig, mergeConfig, Plugin, PluginOption } from 'vite';
 
 import base from '../../build-config/templates/vite/base.vite.config';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const INDEX_HTML_TEMPLATE = resolve(__dirname, './example.index.html');
+
+const template = readFileSync(INDEX_HTML_TEMPLATE, 'utf-8');
 
 export default mergeConfig(
   base,
@@ -9,7 +19,28 @@ export default mergeConfig(
     esbuild: {
       jsx: 'preserve',
     },
+    build: {
+      rollupOptions: {
+        input: resolve(__dirname, './example.index.html'),
+      },
+    },
     plugins: [
+      {
+        name: 'index-html-template',
+        apply: 'serve',
+        configureServer(server): void {
+          server.middlewares.use((req, res, next) => {
+            if (req.url === '/') {
+              res.setHeader('Content-Type', 'text/html');
+              res.write(template, () => {
+                res.end();
+              });
+            } else {
+              next();
+            }
+          });
+        },
+      },
       babel({
         presets: ['@reactive-kit/babel-preset-reactive'],
         ignore: [/\bnode_modules\b/],
