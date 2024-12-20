1982b800d3d210adcfa11de391a2a9047e5e7ce2
Tim Kendrick <timkendrick@gmail.com>
2024-12-20 17:21:18 +0000

    Use async functions in hello world example

--- packages/examples/src/01-hello-world/index.jsx
+++ packages/examples/src/01-hello-world/index.jsx
@@ -1,6 +1,6 @@
 import { render } from '@reactive-kit/dom';
 
-function Main() {
+async function Main() {
   return <h1>Hello, world!</h1>;
 }
 
--- packages/examples/src/01-hello-world/index.mjs
+++ packages/examples/src/01-hello-world/index.mjs
@@ -1,4 +1,4 @@
-function main() {
+async function main() {
   return 'Hello, world!';
 }
 
