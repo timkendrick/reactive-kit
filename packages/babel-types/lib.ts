4458df24130a50b972cb8e7e6c8a477698ab639e
Tim Kendrick <timkendrick@gmail.com>
2025-07-04 16:41:47 +0100

    chore(babel-types): Re-export babel types from module entrypoint
    # Split from commit 131d1cf: chore(babel-types): Re-export babel types from module entrypoint

--- packages/babel-types/lib.ts
+++ packages/babel-types/lib.ts
@@ -1,1 +1,18 @@
-export * from './src/index';
+import type { PluginObj, PluginOptions, PluginPass } from '@babel/core';
+import type * as BabelCore from '@babel/core';
+
+export type { FieldOptions } from '@babel/types';
+export type { Scope, Binding, TraverseOptions } from '@babel/traverse';
+export type { PluginObj, PluginPass } from '@babel/core';
+export type * as types from '@babel/types';
+
+export type Babel = typeof BabelCore;
+export type BabelPlugin<S = PluginPass, O = never> = (babel: Babel, options?: O) => PluginObj<S>;
+export type BabelPluginWithOptions<S = PluginPass, T extends PluginOptions = object> = [
+  BabelPlugin<S>,
+  T,
+];
+export type Node = BabelCore.Node;
+export type NodePath<T = Node> = BabelCore.NodePath<T>;
+
+export { NODE_FIELDS, VISITOR_KEYS } from '@babel/types';
--- packages/babel-types/src/types/ast.ts
+++
@@ -1,1 +0,0 @@
-export { NODE_FIELDS, VISITOR_KEYS, type FieldOptions } from '@babel/types';
