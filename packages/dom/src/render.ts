e9ca2095e9fbcb794ed23174d02a82e462276bd8
Tim Kendrick <timkendrick@gmail.com>
2025-07-11 10:32:44 +0100

    feat(dom): Add optional console logging middleware

--- packages/dom/src/render.ts
+++ packages/dom/src/render.ts
@@ -1,14 +1,24 @@
 import type { JSX } from '@reactive-kit/component';
 import { handlers } from '@reactive-kit/handlers';
 import type { Hashable } from '@reactive-kit/hash';
-import { Runtime } from '@reactive-kit/runtime';
+import { Runtime, type RuntimeMessage } from '@reactive-kit/runtime';
 import { wrapExpression, type Expression } from '@reactive-kit/types';
 import { subscribeAsyncIterator } from '@reactive-kit/utils';
 
+import { consoleLoggerMiddleware } from './middleware';
 import { renderDom, type RenderedNode } from './vdom';
 
-export function render(root: JSX.Element, container: Element | DocumentFragment): Promise<null> {
-  const runtime = new Runtime(handlers);
+export function render(
+  root: JSX.Element,
+  container: Element | DocumentFragment,
+  options?: {
+    log?: boolean;
+  },
+): Promise<null> {
+  const { log = false } = options ?? {};
+  const runtime = new Runtime(handlers, {
+    middleware: log ? consoleLoggerMiddleware<RuntimeMessage>(globalThis.console) : undefined,
+  });
   const results = runtime.subscribe(wrapExpression(root) as Expression<JSX.Element & Hashable>);
   return subscribeAsyncIterator(
     results as AsyncIterator<JSX.Element, null, undefined>,
