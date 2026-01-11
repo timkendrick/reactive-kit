146e8d0785ce3eaf427593fbfc8238657879fd20
Tim Kendrick <timkendrick@gmail.com>
2026-01-11 15:55:05 +0000

    Fix errors in 3rd-party www components

--- packages/www/src/components/magicui/border-beam.tsx
+++ packages/www/src/components/magicui/border-beam.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { motion, MotionStyle, Transition } from "motion/react";
+import { motion, type MotionStyle, type Transition } from "motion/react";
 
 interface BorderBeamProps {
   /**
