1d52ef689d7abb854d0677e268e92dda635e8f66
Tim Kendrick <timkendrick@gmail.com>
2026-02-04 21:45:38 +0000

    Configure www site astro base path

--- packages/www/astro.config.mjs
+++ packages/www/astro.config.mjs
@@ -5,6 +5,10 @@ import { defineConfig } from 'astro/config';
 import viteConfig from './vite.config';
 
 export default defineConfig({
+  base: '/reactive-kit/',
+  build: {
+    assets: 'assets',
+  },
   integrations: [tailwindcss(), react()],
   vite: {
     ...viteConfig,
